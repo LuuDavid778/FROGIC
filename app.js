7a88ce63a4220057bd32e3e1ee86b164fbf099b6
luudavid778 <luudavid778@gmail.com>
2020-03-13 09:17:24 -0700

    Started intropage#2 in object in js

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+var introstate = [
+        {
+            img: "img/logo.svg",
+            header: "<p>Hello <span id=\"nameofuser\">name</span>!<br> My name is Hoppy.</p>",
+            cardtext: "<p>I need your help. <br> Can you build me a car? <br> I need to get to my grandpa's house. <br> My friends will give you the car parts but,  You must help them first!</p>"
+            
+
+        },
+
+        {
+            img: "img/ribbit.svg",
+            header:"<p>Psst!</p><br><p>I'm Mr.Ribbit.",
+            cardtext: "<p>Along our journey, I'll be<br>teaching you the methods of reasoning!  </p>"
+
+        }
+
+];
+
+var introcounter = 0;
 
 function storeName() {
     var userinput = document.getElementById("textbox__input").value;
@@ -10,6 +29,13 @@ function storeName() {
 function startGame() {
     document.getElementById("startpage").style.display = "none";
     document.getElementById("namepage").style.display = "flex";
+}
+
+function forwardIntro(){
+    document.querySelector(".q__text__header").innerHTML = introstate[1].header;
+    document.querySelector(".q__text__header").innerHTML = introstate[1].header;
+    console.log(introstate[1].header)
+
 }
 
 
