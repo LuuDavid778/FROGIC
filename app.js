b0227b5ad1a6f5dff0bec64ea7f177ceb865dbd2
luudavid778 <luudavid778@gmail.com>
2020-03-12 22:57:27 -0700

    Fixed background-color for app and modified p css

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 
 function storeName() {
     var userinput = document.getElementById("textbox__input").value;
-    console.log(userinput);
     document.getElementById("nameofuser").innerHTML = userinput
     document.getElementById("namepage").style.display = "none";
     document.getElementById("intropage").style.display = "flex";
+    document.getElementById("app").style.backgroundColor = "#fdbe6c";
 }
 
 function startGame() {
