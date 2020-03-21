b89a1edcfd45a0ae144ff1cfb7470979ee888381
= <=>
2020-03-20 23:04:03 -0700

    Created cardtext in deductive short summary in app.js

--- app.js
+++ app.js
@@ -10,12 +10,21 @@ var deductindex = 0
 
         {
             img: `<div id="intro__top"><div id="intro__top_caption">You're right!</div><img src="img/dog1_happy_bone.svg"></div>`,
-            header: `<p id="intro__header">Yes, Racer most likely lost his bone.</p>`,
-            cardtext: `<div class="deductive_hint" id="deductive_reasoning_correct">
-            <img src="img/ribbit_hint.svg" class="deductive_hint__ribbit" id="deductive_hint__ribbit_correct_image">
-            <div class="deductive_hint__hint_border" id="deductive_hint__borderline">
-                    <p class="deductive_hint__hint_text" id="deductive_hint__hint_caption">Hint: If 2 premises are true, conclusion is true!</p>
-        </div>`,
+            header: ``,
+            cardtext: `<div id="deductive_short_shummary">
+            <div class="deductive_short_shummary__ribbit">
+            <img src="img/ribbit_hint.svg" class="deductive_hint__ribbit">
+            </div>
+            <div class="deductive_short_shummary__texts">
+            <p id="intro__header">You need <span class="deductive_short_shummary__pink">2 premises</span> to make a conclusion.<br/>
+            Your premises must be <span class="deductive_short_shummary__pink">true</span> in order for your conclusion to be <span class="deductive_short_shummary__pink">true</span>.<br/>
+            Your <span class="deductive_short_shummary__bold">first</span> premise should be a <span class="deductive_short_shummary__pink">general idea</span>.<br/>
+            Your <span class="deductive_short_shummary__bold">second</span> premise should be a <span class="deductive_short_shummary__pink">specific idea</span>.<br/>
+            Lastly, your conclusion should be a <span class="deductive_short_shummary__pink">specific idea</span>.<br/>
+            All together, you used <span class="deductive_short_shummary__last_text">Deductive reasoning!</span></p>
+            </div>
+
+            </div>`,
             bgColor: "#91abff",
             fontSize: "10pt",
             buttontext: "Receive your prize"
